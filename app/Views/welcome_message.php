fbf9851b24bb15cc8ad7963e1bd0e16024bb82ec
angkasa <rezaangkasa@outlook.com>
2023-10-11 01:30:34 +0700

    fixing

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/bootstrap.5.2.3.min.css">
     <script src="/js/jquery-3.3.1.min.js"></script>
